1058ccbe151b60e34ab3085db35f91fa32f9a0a6
Jonas Bergman <jobe0900@student.miun.se>
2014-05-17 12:11:55 +0200

    continuing work on ProductionSite

--- src/com/example/android_projekt/ProductionSiteActivity.java
+++ src/com/example/android_projekt/ProductionSiteActivity.java
@@ -4,25 +4,45 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.os.Build;
 
-public class ProductionSiteActivity extends ActionBarActivity {
+public class ProductionSiteActivity extends ActionBarActivity 
+{
+	public final static String TAG = "Brunst: ProductionSiteActivity";
+	
+	private EditText etOrg;
+	private EditText etPpnr;
+	private EditText etName;
+	private EditText etAddress;
+	private EditText etPostnr;
+	private EditText etPostaddress;
+	private EditText etCoord;
+	private ImageButton ibMap;
+	private ImageButton ibHere;
+	private Button btnSave;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_production_site);
+		
+		findViews();
+		setupClickListeners();
 
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.production_site, menu);
 		return true;
@@ -39,5 +59,61 @@ public class ProductionSiteActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	private void findViews() {
+		etOrg = (EditText) findViewById(R.id.production_site_entry_org);
+		etPpnr = (EditText) findViewById(R.id.production_site_entry_ppnr);
+		etName = (EditText) findViewById(R.id.production_site_entry_name);
+		etAddress = (EditText) findViewById(R.id.production_site_entry_address);
+		etPostnr = (EditText) findViewById(R.id.production_site_entry_postnr);
+		etPostaddress = (EditText) findViewById(R.id.production_site_entry_postaddress);
+		etCoord = (EditText) findViewById(R.id.production_site_entry_coord);
+		ibMap = (ImageButton) findViewById(R.id.production_site_imgbutton_map);
+		ibHere = (ImageButton) findViewById(R.id.production_site_imgbutton_here);
+		btnSave = (Button) findViewById(R.id.production_site_button_save);
+	}
+	
+	private void setupClickListeners() {
+		OnClickListener clickListener = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				switch(v.getId()) {
+				case R.id.production_site_button_save:
+					saveForm();
+					break;
+				case R.id.production_site_imgbutton_map:
+					placeOnMap();
+					break;
+				case R.id.production_site_imgbutton_here:
+					getLocation();
+				}
+				
+			}
+		};
+		
+		ibMap.setOnClickListener(clickListener);
+		ibHere.setOnClickListener(clickListener);
+		btnSave.setOnClickListener(clickListener);
+	}
+
+	protected void saveForm() {
+		// TODO Auto-generated method stub
+		Log.d(TAG, "Save the form");
+		
+	}
+
+	protected void placeOnMap() {
+		// TODO Auto-generated method stub
+		Log.d(TAG, "Place site on map");
+		
+	}
+	
+	protected void getLocation() {
+		// TODO Auto-generated method stub
+		Log.d(TAG, "Get current location");
+		
+	}
+
+	
 
 }
